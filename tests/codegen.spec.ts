bcc40fb1b16631b8f98c214822dc767d80a24488
Akshay <akshayghewari005@gmail.com>
2025-12-16 21:04:31 +0530

    add new file practice2

--- tests/codegen.spec.ts
+++
@@ -1,3 +0,0 @@
-//Codegen- Internal tool available inside playwright
-//Record and playback tool
-
--- utils/Env.ts
+++
@@ -1,5 +0,0 @@
-export const ENV = {
-    baseURL: process.env.BASE_URL,
-    email: process.env.EMAIL,
-    password: process.env.PASSWORD
-}
